b2f89be7476b66e7cac3d79460ff855288c59d8d
Vrutik-Patwa <vrutikpatwa712@gmail.com>
2024-05-24 22:19:49 +0530

    sarting

--- Notes_App/src/App.jsx
+++ Notes_App/src/App.jsx
@@ -1,49 +1,35 @@
-import Split from "react-split";
-import Sidebar from "./Components/Sidebar";
-import Editor from "./Components/Editor";
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [notes, setnewNotes] = useState([]);
+  const [count, setCount] = useState(0)
 
-  function addnewNote(e) {
-    // e.preventDefault();
-    const newNote = {
-      id: nanoid(),
-      body: "#Write Something Here",
-    };
-    setnewNotes((prevState) => {
-      return [newNote, ...prevState];
-    });
-
-    console.log(notes);
-  }
   return (
-    <main className="h-screen bg-back">
-      {notes.length > 0 ? (
-        <Split
-          className="flex"
-          // Its Basically a dependency in react use to create a grid like layout
-          sizes={[30, 70]}
-          direction="horizontal"
-        >
-          <Sidebar newNote={addnewNote} />
-          <Editor />
-        </Split>
-      ) : (
-        <div className="h-screen flex flex-col justify-center items-center">
-          <h1 className="text-3xl font-semibold ">You have no notes</h1>
-          <button
-            className="text-xl mt-6 p-3 rounded-md bg-but text-white"
-            onClick={() => addnewNote()}
-          >
-            Create one new
-          </button>
-        </div>
-      )}
-    </main>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
